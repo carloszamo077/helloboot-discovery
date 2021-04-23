75a56944a1aac653f8866d69acbb7b8313f73237
Carlos Zamora Ramírez <206990505@bancobcr.com>
2021-04-23 15:55:50 -0600

    Version Inicial

---
+++ src/main/java/com/example/hellobootdiscovery/CalculatorController.java
@@ -0,0 +1,32 @@
+package com.example.hellobootdiscovery;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+/**
+ * @author kameshs
+ */
+@RestController
+@Slf4j
+public class CalculatorController {
+	private final CalculatorService calculatorService;
+
+	private static final Logger log = LoggerFactory.getLogger(CalculatorController.class);
+
+	public CalculatorController(CalculatorService calculatorService) {
+		this.calculatorService = calculatorService;
+	}
+
+	@GetMapping("/randomadd")
+	public String randomAdd() {
+		log.info("Calling Calculator Service: simplecalculator");
+		Random random = new Random();
+		return calculatorService.add(random.nextInt(), random.nextInt());
+	}
+}
